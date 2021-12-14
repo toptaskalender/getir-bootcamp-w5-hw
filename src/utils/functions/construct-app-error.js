f4f4d11f89cd30b4ecb4b413fd0adc3d81d4a29b
toptaskalender <toptaskalender@gmail.com>
2021-12-14 23:17:13 +0300

    Update 'constructAppError' util func

--- src/utils/functions/construct-app-error.js
+++ src/utils/functions/construct-app-error.js
@@ -1,21 +1,28 @@
 const { AppError } = require('../classes')
 
 function constructAppError(err) {
-  let msg;
+  let statusCode, msg
 
   if (err.code === 11000) {
     const errorField = Object.keys(err.keyPattern)[0]
 
-    msg = `${errorField[0].toUpperCase() + errorField.slice(1)} should be unique.`
+    statusCode  = 400
+    msg         = `${errorField[0].toUpperCase() + errorField.slice(1)} should be unique.`
   }
   else if (err.name === 'ValidationError') {
     const { errors }  = err
     const errorFields = Object.keys(errors)
 
-    msg = errorFields.map(key => errors[key].message).join(' ')
+    statusCode  = 400
+    msg         = errorFields.map(key => errors[key].message).join(' ')
+  }
+  else if (err.code === 'EAUTH') {
+
+    statusCode  = 500
+    msg         = 'Cannot send password reset email.'
   }
 
-  return new AppError(400, msg)
+  return new AppError(statusCode, msg)
 }
 
 module.exports = constructAppError

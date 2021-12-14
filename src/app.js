1360f7aedc35be4751cbdfde4a4af3670764c567
toptaskalender <toptaskalender@gmail.com>
2021-12-14 23:08:34 +0300

    Add api version to root path, and add global err message for nonexistent api paths

--- src/app.js
+++ src/app.js
@@ -5,12 +5,18 @@ const {
 const {
   errorHandler
 }                 = require('./middlewares')
+const {
+  AppError
+}                 = require('./utils/classes')
 
 const app = express()
 
 app.use(express.json())
+app.use('/api/v1', authRouter)
 
-app.use('/', authRouter)
+app.all('*', (req, res, next) => {
+  return next(new AppError(500, `${req.originalUrl} does not exist on our server.`))
+})
 
 app.use(errorHandler)
 

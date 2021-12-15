a9f872097e7485cdb6521ece94be3ad746034c04
toptaskalender <toptaskalender@gmail.com>
2021-12-15 14:36:59 +0300

    Implement morgan

--- src/app.js
+++ src/app.js
@@ -1,19 +1,24 @@
-const express     = require('express')
+const express         = require('express')
+const morgan          = require('morgan')
+const {
+  resLogWriteStream
+}                     = require('./logs')
 const {
   authRouter,
   usersRouter
-}                 = require('./routes')
+}                     = require('./routes')
 const {
   errorHandler,
   verifyAuth
-}                 = require('./middlewares')
+}                     = require('./middlewares')
 const {
   AppError
-}                 = require('./utils/classes')
+}                     = require('./utils/classes')
 
 const app = express()
 
 app.use(express.json())
+app.use(morgan('combined', { stream: resLogWriteStream }))
 
 app.use('/api/v1', authRouter)
 app.use(

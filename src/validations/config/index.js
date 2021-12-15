feac3a59d1d9a4cf87746fff015009b600912679
toptaskalender <toptaskalender@gmail.com>
2021-12-15 20:15:52 +0300

    Update validation config

--- src/validations/config/index.js
+++ src/validations/config/index.js
@@ -1,2 +1,6 @@
-module.exports.PASSWORD_MIN  = 8
-module.exports.USER_ROLES    = ['user', 'admin']
+module.exports.USER_ROLES                           = ['user', 'admin']
+module.exports.USER_PASSWORD_MIN                    = 8
+
+module.exports.PRODUCT_CATEGORIES                   = ['Beverages', 'Personal Care', 'Food', 'Home Care']
+module.exports.PRODUCT_DEFAULT_UNIT_PRICE_CURRENCY  = 'TRY'
+module.exports.PRODUCT_CURRENCIES                   = ['TRY', 'USD', 'EUR']

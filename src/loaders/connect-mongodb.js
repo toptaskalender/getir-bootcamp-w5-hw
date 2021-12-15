7615f550d40218309d066ae0a29c5da0c4319618
toptaskalender <toptaskalender@gmail.com>
2021-12-15 12:54:40 +0300

    Change database connection message

--- src/loaders/connect-mongodb.js
+++ src/loaders/connect-mongodb.js
@@ -3,6 +3,7 @@ const mongoose  = require('mongoose')
 async function connectMongoDB() {
   try {
     await mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`)
+    console.log(`Database connected on port : ${process.env.DB_PORT}  🟢`)
   } catch (e) {
     console.error(e)
   }

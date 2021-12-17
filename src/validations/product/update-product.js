12e1120bb8ab9f46e52983b43d8a23de0660741d
toptaskalender <toptaskalender@gmail.com>
2021-12-17 18:58:25 +0300

    Update 'updateProductValidation'

--- src/validations/product/update-product.js
+++ src/validations/product/update-product.js
@@ -4,36 +4,29 @@ const {
   PRODUCT_DEFAULT_UNIT_PRICE_CURRENCY,
   PRODUCT_CURRENCIES
 }                   = require('../config')
+const {
+  createErrors
+}                   = require('../../utils/functions')
 
 const updateProductValidation = Joi.object({
   name: Joi
     .string()
-    .messages({
-      'string.base'   : 'Product name must be a string.',
-    }),
+    .error(createErrors),
 
   category: Joi
     .string()
     .valid(...PRODUCT_CATEGORIES)
-    .messages({
-      'string.base'   : 'Product category must be a string.',
-      'any.only'      : `Product category must be either ${PRODUCT_CATEGORIES.join(', ')}.`
-    }),
+    .error(createErrors),
   
   unitPrice: Joi
     .number()
-    .messages({
-      'number.base'   : 'Product unit price must be a number.',
-    }),
+    .error(createErrors),
 
   unitPriceCurrency: Joi
     .string()
     .default(PRODUCT_DEFAULT_UNIT_PRICE_CURRENCY)
     .valid(...PRODUCT_CURRENCIES)
-    .messages({
-      'string.base'   : 'Product unit price currency must be a string.',
-      'any.only'      : `Product unit price currency must be either ${PRODUCT_CURRENCIES.join(', ')}.`
-    })
+    .error(createErrors)
   
 })
 

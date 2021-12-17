f7e1aeff184349b54ce7ba81544dac340a5add8e
toptaskalender <toptaskalender@gmail.com>
2021-12-17 18:45:17 +0300

    Update 'resetPasswordValidation'

--- src/validations/auth/reset-password.js
+++ src/validations/auth/reset-password.js
@@ -2,22 +2,22 @@ const Joi       = require('joi')
 const {
   USER_PASSWORD_MIN
 }               = require('../config')
+const {
+  createErrors
+}               = require('../../utils/functions')
 
 const resetPasswordValidation = Joi.object({
   password: Joi
     .string()
     .min(USER_PASSWORD_MIN)
     .required()
-    .messages({
-      'string.min'    : `Password must be at least ${USER_PASSWORD_MIN} character long.`,
-      'string.base'   : 'Password must be a string.',
-      'any.required'  : 'Password is a required field.'
-    }),
+    .error(createErrors),
 
   passwordConfirm: Joi
     .ref('password')
 
 })
   .with('password', 'passwordConfirm')
+  .error(createErrors)
 
 module.exports = resetPasswordValidation

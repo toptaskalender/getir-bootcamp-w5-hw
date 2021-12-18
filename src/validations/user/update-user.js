6e377939dfd1e1800e8dfd74725a601f6cdff064
toptaskalender <toptaskalender@gmail.com>
2021-12-19 01:02:12 +0300

    Update 'updateUserValidation'

--- src/validations/user/update-user.js
+++ src/validations/user/update-user.js
@@ -7,6 +7,14 @@ const {
 }                 = require('../../utils/functions')
 
 const updateUserValidation = Joi.object({
+  firstName: Joi
+    .string()
+    .error(createErrors),
+
+  lastName: Joi
+    .string()
+    .error(createErrors),
+
   role: Joi
     .string()
     .valid(...USER_ROLES)

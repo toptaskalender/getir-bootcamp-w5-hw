8b3080b595ba2ac4961e2ff3ed8531312b230504
toptaskalender <toptaskalender@gmail.com>
2021-12-19 20:58:55 +0300

    Add 'findById' service to product service

--- src/services/product.js
+++ src/services/product.js
@@ -1,6 +1,20 @@
 const BaseService = require('./base')
 const { Product } = require('../models')
 
-class ProductService extends BaseService {}
+class ProductService extends BaseService {
+  async findById(id, fieldBy) {
+    return this.model
+      .findById(id)
+      .select(fieldBy)
+      .populate({
+        path: 'comments',
+        select: 'user body title rating',
+        populate : {
+          path : 'user',
+          select: 'firstName'
+        }
+      })
+  }
+}
 
 module.exports = new ProductService(Product)

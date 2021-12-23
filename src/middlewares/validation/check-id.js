303bd156f043d735509878eb8617bc06a6ad1aa9
toptaskalender <toptaskalender@gmail.com>
2021-12-23 15:44:56 +0300

    Update error message of checkId util func

--- src/middlewares/validation/check-id.js
+++ src/middlewares/validation/check-id.js
@@ -4,7 +4,7 @@ async function checkId (req, res, next, id) {
   const regex     = /^[0-9a-fA-F]{24}$/
   const isValidId = regex.test(id)
 
-  if (!isValidId) return next(new AppError(400, 'Invalid id parameter. Please provide correct information.'))
+  if (!isValidId) return next(new AppError(400, 'Invalid id parameter. Please provide a valid id.'))
 
   next()
 }

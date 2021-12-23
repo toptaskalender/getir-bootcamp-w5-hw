4b35ecdf41e416c6ac640615d00de6854c69c8cb
toptaskalender <toptaskalender@gmail.com>
2021-12-23 13:03:50 +0300

    Create 'findSubcategories' categoryService func

--- src/services/category.js
+++ src/services/category.js
@@ -1,6 +1,29 @@
 const BaseService   = require('./base')
 const { Category }  = require('../models')
 
-class categoryService extends BaseService {}
+class categoryService extends BaseService {
+  async findSubcategories() {
+    return this.model.aggregate([
+      {
+        $unwind: "$subcategories" 
+      },
+      {
+        $group: {
+          _id: '$subcategories',
+        }
+      },
+      {
+        $addFields: {
+          name: '$_id',
+        }
+      },
+      {
+        $project: {
+          _id: 0,
+        }
+      }
+    ])
+  }
+}
 
 module.exports = new categoryService(Category)

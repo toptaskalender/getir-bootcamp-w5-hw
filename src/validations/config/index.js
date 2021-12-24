d4e6b0e794102f2481b0ec2a2dd8772550252f9f
toptaskalender <toptaskalender@gmail.com>
2021-12-24 15:20:34 +0300

    Update validation configs

--- src/validations/config/index.js
+++ src/validations/config/index.js
@@ -5,7 +5,7 @@ module.exports.USER_EMAIL_OPTIONS                     = { minDomainSegments: 2, 
 module.exports.MAX_COMMENT_CHARACTER                  = 200
 
 module.exports.PRODUCT_CATEGORIES                     = ['Beverages', 'Food']
-module.exports.PRODUCT_SUB_CATEGORIES                 = ['Water', 'Tea', 'Pasta', 'Oil']
+module.exports.PRODUCT_SUB_CATEGORIES                 = ['Water', 'Tea', 'Pasta', 'Oil', 'Soda']
 module.exports.PRODUCT_DEFAULT_ENTITY_PRICE_CURRENCY  = 'TRY'
 module.exports.PRODUCT_ENTITY_PRICE_CURRENCIES        = ['TRY', 'USD', 'EUR']
 module.exports.PRODUCT_ENTITY_AMOUNT_UNITS            = ['g', 'kg', 'L', 'ml', 'ct']
